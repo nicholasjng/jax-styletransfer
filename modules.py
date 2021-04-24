c761c0aa775f3bfc879c018a8342f42ca647f00b
Nicholas Junge <nicholas.junge@web.de>
2021-04-24 13:04:38 +0200

    Better typehints, prototype normalization input layer

--- modules.py
+++ modules.py
@@ -9,7 +9,7 @@ imagenet_mean = jnp.array([0.485, 0.456, 0.406])
 imagenet_std = jnp.array([0.229, 0.224, 0.225])
 
 
-def gram_matrix(x: jnp.array):
+def gram_matrix(x: jnp.ndarray):
     """Computes Gram Matrix of an input array x."""
     # N-C-H-W format
     n, c, h, w = x.shape
@@ -50,16 +50,26 @@ class ContentLoss(hk.Module):
 
 
 class Normalization(hk.Module):
-    # create a module normalizing input image so we can easily put it in a
-    # hk.Sequential
-    def __init__(self, mean, std):
-        super(Normalization, self).__init__()
+    # create a module normalizing the input image
+    # so we can easily put it into a hk.Sequential
+    def __init__(self, image: jnp.ndarray, mean: jnp.ndarray, std: jnp.ndarray,
+                 name: Optional[str] = None):
+        super(Normalization, self).__init__(name=name)
+
+        # save image to make it a trainable parameter
+        self.image = image
+
         # reshape mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [N x C x H x W].
         self.mean = jnp.array(mean).reshape(-1, 1, 1)
         self.std = jnp.array(std).reshape(-1, 1, 1)
 
     def forward(self, x):
-        # normalize input
-        # TODO: Add image variable
-        return (x - self.mean) / self.std
+        # throw away the input and (re-)use the tracked parameter instead
+        # this assures that the image is actually styled
+        img = hk.get_parameter("image",
+                               shape=self.image.shape,
+                               dtype=self.image.dtype,
+                               init=hk.initializers.Constant(self.image))
+
+        return (img - self.mean) / self.std

2921d737dc1cf7a533d7f1d93900e6c7ee2ad36b
Nicholas Junge <nicholas.junge@web.de>
2021-04-26 17:02:45 +0200

    Add model pruning, kernel shape inference, fix bugs

--- models.py
+++ models.py
@@ -27,12 +27,13 @@ def get_model_params(fp: str, verbose: bool = False) -> Parameters:
     # layers (weights + biases) arranged in h5.Groups
     for layer_name, group in hf.items():
         param_dict = dict()
-        print(f"Layer name: {layer_name}")
+        if verbose:
+            print(f"Layer name: {layer_name}")
         for k, v in group.items():
             values = jnp.array(v)
             # add two dummy dims to bias layers
             if len(values.shape) == 1:
-                values.reshape((-1, 1, 1))
+                values = jnp.expand_dims(values, (1, 2))
 
             if verbose:
                 print(f"Parameter name: {k} Parameter shape: {v.shape}")
@@ -61,16 +62,17 @@ def augmented_vgg19(fp: str,
                     style_layers: List[str] = None,
                     pooling: str = "avg") -> hk.Sequential:
     """Build a VGG19 network augmented by content and style loss layers."""
-
-    if pooling.lower() not in ["avg", "max"]:
+    pooling = pooling.lower()
+    if pooling not in ["avg", "max"]:
         raise ValueError("Pooling method not recognized")
 
     params = get_model_params(fp=fp)
 
     # prepend a normalization layer
-    layers = [Normalization(content_image, mean, std, "norm")]
+    layers = [Normalization(content_image, mean, std, "norm")]  # []
 
-    n_conv, n_pools, c_losses, s_losses = 1, 1, 1, 1
+    # tracks number of conv layers
+    n = 0
 
     # desired depth layers to compute style/content losses :
     content_layers = content_layers or []
@@ -78,28 +80,31 @@ def augmented_vgg19(fp: str,
 
     model = hk.Sequential(layers=layers)
 
-    for k, p_dict in params:
+    for k, p_dict in params.items():
         if "pool" in k:
             if pooling == "avg":
+                # exactly as many as pools as convolutions
                 layers.append(hk.AvgPool(window_shape=2,
                                          strides=2,
                                          padding="VALID",
                                          channel_axis=1,
-                                         name=f"avg_pool_{n_pools}"))
+                                         name=f"avg_pool_{n}"))
             else:
                 layers.append(hk.MaxPool(window_shape=2,
                                          strides=2,
                                          padding="VALID",
                                          channel_axis=1,
-                                         name=f"max_pool_{n_pools}"))
-            n_pools += 1
-
+                                         name=f"max_pool_{n}"))
         elif "conv" in k:
-            name = f"conv_{n_conv}"
+            n += 1
+            name = f"conv_{n}"
 
-            in_ch, kernel_h, kernel_w, out_ch = p_dict["w"].shape
+            kernel_h, kernel_w, in_ch, out_ch = p_dict["w"].shape
 
-            kernel_shape = (kernel_h, kernel_w)
+            # VGG only has square conv kernels
+            assert kernel_w == kernel_h, \
+                "VGG19 is assumed to have square conv kernels"
+            kernel_shape = kernel_h
 
             layers.append(hk.Conv2D(
                 output_channels=out_ch,
@@ -111,23 +116,29 @@ def augmented_vgg19(fp: str,
                 b_init=hk.initializers.Constant(p_dict["b"]),
                 name=name))
 
-            n_conv += 1
+            if name in style_layers:
+                model.layers = tuple(layers)
+                style_target = model(style_image, is_training=False)
+                layers.append(StyleLoss(target=style_target,
+                                        name=f"style_loss_{n}"))
 
             if name in content_layers:
                 model.layers = tuple(layers)
-                style_target = model(content_image)
-                layers.append(StyleLoss(target=style_target,
-                                        name=f"style_loss_{s_losses}"))
-                s_losses += 1
-
-            if name in style_layers:
-                model.layers = tuple(layers)
-                content_target = model(style_image)
+                content_target = model(content_image, is_training=False)
                 layers.append(ContentLoss(target=content_target,
-                                          name=f"content_loss_{c_losses}"))
-                c_losses += 1
+                                          name=f"content_loss_{n}"))
 
             layers.append(jax.nn.relu)
+
+    # this modifies our n from before in place
+    for n in range(len(layers) - 1, -1, -1):
+        if isinstance(layers[n], (StyleLoss, ContentLoss)):
+            break
+
+    # break off after last content loss layer
+    layers = layers[:(n + 1)]
+    # prepend normalization layer
+    # layers.insert(0, Normalization(content_image, mean, std, "norm"))
 
     model.layers = tuple(layers)
 
